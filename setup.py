03f4db78bfa7abe07d309964cb5fb3a322967408
Daniel Lawrence <dannyla@linux.com>
2013-09-11 20:52:02 +1000

    added setup.py for pip package

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import os
+import setuptools
+
+README = "/".join([os.path.dirname(__file__), "README.md"])
+
+with open(README) as file:
+        long_description = file.read()
+
+setup(
+    name='total',
+    version='0.0.3',
+    description='Simple AWK tasks, made awesome',
+    author='Danny Lawrence',
+    author_email='dannyla@linux.com',
+    url='https://github.com/daniellawrence/total',
+    packages=['total'],
+    long_description=long_description,
+    entry_points = {
+        'console_scripts': [
+            'total = total.total:main'
+            ]
+        }
+)
